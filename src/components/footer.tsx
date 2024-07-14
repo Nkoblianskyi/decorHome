9b1c47628e3d90a4f3fd38883bb5997fb1224db1
Nykyta Koblianskyi <ok.service.company@gmail.com>
2024-07-14 20:13:09 +0200

    main

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,17 @@
+import { Logo } from "../elements/logo"
 import { Menu } from "../elements/menu"
 
 export const Footer = () => {
     return (
         <div className="footer">
+            <Logo/>
             <Menu/>
-            {/* <address>
+            <address className="footer-address">
                 <a href="google.com">ul.Wislicka 5</a>
-            </address> */}
+            </address>
+            <div className="footer-producer">
+                <p className="footer-producer-text">© 2024 Nykyta Koblianskyi- All rights reserved. </p>
+            </div>
         </div>
     )
 }
